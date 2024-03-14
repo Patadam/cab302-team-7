0065d76ec5a55904f50b1407f07e037c5cf06e6b
Alireza Saeb <asaeb95@gmail.com>
2024-03-14 11:34:02 +1000

    hi

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Welcome to CAB302!");
         System.out.println("Can you see me");
+        system.out.println("Welcome to CAB302");
 
         System.out.println("testing here");
         //works maybe
