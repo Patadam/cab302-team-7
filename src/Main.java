240f9168f5454182a75f728cd959bbee71605648
breepedersen <breepedersen670@gmail.com>
2024-03-14 11:00:17 +1000

    test

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Welcome to CAB302!");
+
+        System.out.println("How do you use the library printers I'm so confused");
     }
 }
