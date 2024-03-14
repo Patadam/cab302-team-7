477acfdc1bac05bf9e6ad395ad1e37bd848a7e5f
Vin Boyle <vin.boyle@connect.qut.edu.au>
2024-03-14 11:05:01 +1000

    Vin2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Welcome to CAB302!");
+        System.out.println("Can you see me?");
     }
 }
