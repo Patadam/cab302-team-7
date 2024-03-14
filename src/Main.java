8001a6a92f989e4df50fc1397d420f8175600fb1
Vin Boyle <vin.boyle@connect.qut.edu.au>
2024-03-14 11:40:22 +1000

    Vin4444

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,17 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Welcome to CAB302!");
         System.out.println("Can you see me");
-        system.out.println("Welcome to CAB302");
+        System.out.println("Welcome to CAB302");
 
         System.out.println("testing here");
         //works maybe
+
+
+
+        System.out.println("HIIIIIIII");
+
+
+
 
 
         System.out.println("How do you use the library printers I'm so confused");
