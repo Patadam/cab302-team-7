329e8ff9731ec2e3aed3b0a7daeb1e7885f475bd
Vin Boyle <vin.boyle@connect.qut.edu.au>
2024-04-02 10:27:25 +1000

    Added function of ContactUs button(disappear)

--- src/main/java/com/example/addressbook/HelloApplication.java
+++ src/main/java/com/example/addressbook/HelloApplication.java
@@ -4,14 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     // Constants defining the window title and size
     public static final String TITLE = "Home";
     public static final int WIDTH = 900;
-    public static final int HEIGHT = 450;
+    public static final int HEIGHT = 550;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
--- src/main/java/com/example/addressbook/HelloController.java
+++ src/main/java/com/example/addressbook/HelloController.java
@@ -2,37 +2,41 @@ package com.example.addressbook;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-import java.net.URL;
-
 public class HelloController {
-    @FXML
-    private Label welcomeText;
-    @FXML
-    private Label Navigete_to_1;
-    @FXML
-    private Label Navigete_to_2;
-    @FXML
-    private Label Navigete_to_3;
-    @FXML
-    private Label Navigete_to_4;
-    @FXML
-    private ImageView imageView;
+    @FXML    private Label welcomeText;
+    @FXML    private Label Navigete_to_1;
+    @FXML    private Label Navigete_to_2;
+    @FXML    private Label Navigete_to_3;
+    @FXML    private Label Navigete_to_4;
+    @FXML    private ImageView imageView;
+    @FXML    private Button contactUsButton;
+    @FXML    private Label contactUsLabel;
 
+    //Load image in Home GUI
     public void initialize(){
-        // Load image
-        Image image = new Image(getClass().getResourceAsStream("/com/example/addressbook/example_image.png"));
-        // Set image to ImageView
-        imageView.setImage(image);
+        Image image = new Image(getClass().getResourceAsStream("/com/example/addressbook/Computer.png"));
+        imageView.setImage(image);  //Loading an image with ImageView
     }
 
-
+    //Hello! button Click Setup
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    //ContactUs! button Click Setup
+    @FXML       //Bottom of the Home GUI button operation
+    protected void onContactusButtonClick() {
+        contactUsLabel.setText("Team7.qut.edu.au");
+        contactUsButton.setVisible(false);  //after click the button disappear
+        contactUsLabel.setVisible(true);    //replace the button with setText message
+    }
+
+    //Navigation Button Setup
     @FXML
     protected void onHelloButtonClick1() {
         Navigete_to_1.setText("Take me");
