c9f28683d8a31337c064b10ad83c1c0fa889c8bb
Patrick <42367024+Patadam@users.noreply.github.com>
2024-04-10 18:15:37 +1000

    refactor: refactored and added IUserDAO interface

--- src/main/java/com/wellnessapp/model/UserDAO.java
+++ src/main/java/com/wellnessapp/model/UserDAO.java
@@ -1,16 +1,20 @@
-package com.example.addressbook;
+package com.wellnessapp.model;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.Connection;
+import java.sql.Statement;
 
-public class UserDAO {
+public class UserDAO implements IUserDAO {
     private Connection connection;
 
     public UserDAO() {
         connection = DatabaseConnection.getInstance();
+        createTable();
     }
 
-    public void createTable() {
+    private void createTable() {
         try {
             Statement createTable = connection.createStatement();
             createTable.execute(
@@ -23,15 +27,19 @@ public class UserDAO {
             System.err.println(ex);
         }
     }
+    @Override
     public void insert(User user) {
         // Todo Later: Create a PreparedStatement to run the INSERT query
     }
+    @Override
     public void update(User user) {
         // Todo Later: Create a PreparedStatement to run the UPDATE query
     }
-    public void delete(String username) {
+    @Override
+    public void delete(User user) {
         // Todo Later: Create a PreparedStatement to run the DELETE query
     }
+    @Override
     public List<User> getAll() {
         List<User> accounts = new ArrayList<>();
         // Todo Later: Create a Statement to run the SELECT * query
