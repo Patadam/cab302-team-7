f83f1373ab2ed9a6b26de075fe2cdf4a2576ff93
Patrick <42367024+Patadam@users.noreply.github.com>
2024-04-19 09:49:24 +1000

    build: add required dependencies

---
+++ README.md
@@ -0,0 +1,1 @@
+# CAB302 - Wellness App
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,11 +2,15 @@ module com.wellnessapp {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires password4j;
+    requires org.controlsfx.controls;
 
 
+    opens com.wellnessapp.controller to javafx.fxml;
     opens com.wellnessapp to javafx.fxml;
+
     exports com.wellnessapp;
     exports com.wellnessapp.controller;
     exports com.wellnessapp.model;
-    opens com.wellnessapp.controller to javafx.fxml;
+    exports com.wellnessapp.operations;
 }
