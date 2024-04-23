20c7006e01aa28bb1efd2ee2c7b9cf2b58a8761f
Patrick <42367024+Patadam@users.noreply.github.com>
2024-04-23 16:54:09 +1000

    feat: WIP on mood entry popup

--- src/main/java/com/wellnessapp/controller/MoodPopupController.java
+++ src/main/java/com/wellnessapp/controller/MoodPopupController.java
@@ -6,10 +6,24 @@ import com.wellnessapp.model.MoodDAO;
 import com.wellnessapp.model.MoodEntry;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.Date;
 
 public class MoodPopupController {
     @FXML
-    private Button testBtn;
+    private Button createEntryBtn;
+    @FXML
+    private Button cancelEntryBtn;
+    @FXML
+    private TextField timeHours;
+    @FXML
+    private TextField timeMinutes;
+
+    final private LocalDateTime now = LocalDateTime.now();
 
     /**
      * Starter method that runs on page mount
@@ -17,6 +31,11 @@ public class MoodPopupController {
     public void initialize(){
         System.out.println("started");
         initialiseDatabase();
+
+        final String hours = now.format(DateTimeFormatter.ofPattern("hh"));
+        final String minutes = now.format(DateTimeFormatter.ofPattern("mm"));
+        timeHours.setText(hours);
+        timeMinutes.setText(minutes);
     }
 
     private void initialiseDatabase() {
@@ -24,10 +43,14 @@ public class MoodPopupController {
     }
 
     @FXML
-    protected void onTestClicked() {
+    protected void onCreateEntry() {
         // run code
         System.out.println("Btn Clicked");
         MoodEntry entry = new MoodEntry(MoodType.HAPPY);
+    }
+    @FXML
+    protected void onCancelEntry() {
+        System.out.println("Mood Popup - cancelled entry");
     }
 
 }
