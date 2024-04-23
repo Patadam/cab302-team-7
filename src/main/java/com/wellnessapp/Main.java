255f58dd197e3ec60d7f665d9b088dcdb63af106
Patrick <42367024+Patadam@users.noreply.github.com>
2024-04-23 15:41:09 +1000

    feat(styles): add support for css styling

--- src/main/java/com/wellnessapp/Main.java
+++ src/main/java/com/wellnessapp/Main.java
@@ -14,8 +14,9 @@ public class Main extends Application {
     public static final int HEIGHT = 550;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
+        scene.getStylesheets().add(getClass().getResource("global.css").toExternalForm());
         stage.setTitle(TITLE);
         stage.setScene(scene);
         stage.show();
