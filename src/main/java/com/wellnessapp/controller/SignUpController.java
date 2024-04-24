26a32cbf88da3086df8151d0870154f66331480f
Jonathon Foo <jonfoo108@gmail.com>
2024-04-24 10:59:55 +1000

    Sign-up and Login pages created Need to create function for Database Test the link between pages Redo the FXML as not what we want design wise

---
+++ src/main/java/com/wellnessapp/controller/SignUpController.java
@@ -0,0 +1,47 @@
+package com.wellnessapp.controller;
+
+import com.wellnessapp.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SignUpController {
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private PasswordField confirmPasswordField;
+
+    @FXML
+    private Hyperlink loginLink;
+
+    @FXML
+    private void handleSignUp(ActionEvent event) {
+
+        //this method should add the account to database and return user to the homepage
+        // this method must also compare the passwordField and confirmPasswordField for them to be equal
+
+        String email = emailField.getText();
+        String password = passwordField.getText();
+        String confirmPassword = confirmPasswordField.getText();
+    }
+
+    @FXML
+    private void goToLoginPage(ActionEvent event) throws IOException {
+        Stage stage = (Stage) loginLink.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+    }
+}
