283dc19027362b714651cfc5d47a68d398cd5315
Vin Boyle <vin.boyle@connect.qut.edu.au>
2024-04-28 19:33:24 +1000

    Created WellnessTip page

---
+++ src/test/DatabaseTest.java
@@ -0,0 +1,13 @@
+import com.wellnessapp.model.*;
+
+import org.junit.jupiter.api.*;
+import java.sql.Connection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class DatabaseTest {
+    @Test
+    public void testConnection() {
+        Connection conn = DatabaseConnection.getInstance();
+        assertEquals(true, conn != null);
+    }
+}
