5968e8117e3885c07d5665540e886241086b5f29
Patrick <42367024+Patadam@users.noreply.github.com>
2024-04-29 13:29:00 +1000

    feat: create mood manager to control logical organisation for moods

---
+++ src/main/java/com/wellnessapp/model/MoodManager.java
@@ -0,0 +1,18 @@
+package com.wellnessapp.model;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MoodManager {
+    private IMoodDAO moodDAO;
+
+    public MoodManager(IMoodDAO moodDAO) {
+        this.moodDAO = moodDAO;
+    }
+
+    public void addMoodEntry(MoodEntry entry) throws SQLException { moodDAO.Create(entry); }
+
+    public List<MoodEntry> getAllEntries() {
+        return moodDAO.getAllEntries();
+    }
+}
