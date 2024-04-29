1982b43451eca5c7dbe3a5370ad51b554b789c3d
Patrick <42367024+Patadam@users.noreply.github.com>
2024-04-29 13:33:10 +1000

    feat: working - allow creating mood entries from popup

--- src/main/java/com/wellnessapp/controller/MoodPopupController.java
+++ src/main/java/com/wellnessapp/controller/MoodPopupController.java
@@ -2,47 +2,42 @@ package com.wellnessapp.controller;
 
 import com.wellnessapp.Main;
 import com.wellnessapp.enums.MoodType;
-
 import com.wellnessapp.model.MoodDAO;
 import com.wellnessapp.model.MoodEntry;
+import com.wellnessapp.model.MoodManager;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
-import javafx.stage.Window;
 
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class MoodPopupController {
+public class MoodPopupController extends BaseController {
     @FXML private Button createEntryBtn;
     @FXML private Button cancelEntryBtn;
     @FXML private TextField timeHours;
     @FXML private TextField timeMinutes;
     @FXML private ImageView moodIcon;
-
-    private Stage stage;
-
-    //private Stage stage;
-
+    @FXML private ComboBox moodValue;
+    private MoodManager moodManager;
     final private LocalDateTime now = LocalDateTime.now();
 
     /**
      * Starter method that runs on page mount
      */
     public void initialize(){
-        //System.out.println("started");
-        System.out.println("[mood] created new popup");
-        initialiseDatabase();
+        moodManager = new MoodManager(new MoodDAO());
 
         final String hours = now.format(DateTimeFormatter.ofPattern("hh"));
         final String minutes = now.format(DateTimeFormatter.ofPattern("mm"));
         timeHours.setText(hours);
         timeMinutes.setText(minutes);
 
-        //stage = (Stage) cancelEntryBtn.getScene().getWindow();
         loadImages();
     }
 
@@ -51,22 +46,38 @@ public class MoodPopupController {
         moodIcon.setImage(image);
     }
 
-    private void initialiseDatabase() {
-        MoodDAO moodDAO = new MoodDAO();
+    @FXML
+    protected void onCreateEntry() throws SQLException {
+        final boolean isInputValid = validateInputs();
+        if (isInputValid) {
+            System.out.println();
+            final MoodType mood = MoodType.valueOf(moodValue.getValue().toString().toUpperCase());
+            MoodEntry entry = new MoodEntry(mood);
+            moodManager.addMoodEntry(entry);
+            getStage().hide();
+        }
+    }
+
+    private boolean validateInputs() {
+        if (moodValue.getValue() == null) {
+            return false;
+        }
+        return true;
     }
 
     @FXML
-    protected void onCreateEntry() {
-        // run code
-        //System.out.println("Btn Clicked");
-        MoodEntry entry = new MoodEntry(MoodType.HAPPY);
+    protected void onCancelEntry() {
+        getStage().hide();
     }
 
-    public void setStage(Stage stage) {
-        this.stage = stage;
-        cancelEntryBtn.setOnAction(event -> {
-            stage.hide();
-        });
+    @FXML
+    protected void onValidateHours(){
+        //String text = timeHours.getText();
+        //if (text.matches("^\\\\d{1,2}D\\\\d{1,2}H\\\\d{1,2}M$")) {
+        //    System.out.println("Valid hours");
+        //} else {
+        //   final String hours = now.format(DateTimeFormatter.ofPattern("hh"));
+        //    timeHours.setText(hours);
+        //}
     }
-
 }
