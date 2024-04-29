55ccd354b5dd02e5dde3de1c96b1d1e23e1780a0
Patrick <42367024+Patadam@users.noreply.github.com>
2024-04-29 13:31:03 +1000

    refactor: improved code clarity and exception tree

--- src/main/java/com/wellnessapp/model/IMoodDAO.java
+++ src/main/java/com/wellnessapp/model/IMoodDAO.java
@@ -1,12 +1,13 @@
 package com.wellnessapp.model;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 public interface IMoodDAO {
 
-     void Create(MoodEntry entry);
+     void Create(MoodEntry entry) throws SQLException;
      void Delete(MoodEntry entry);
-     List<MoodEntry> getAll();
+     List<MoodEntry> getAllEntries();
 }
 
