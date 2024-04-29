8e260cd1ec9cb637263d64bcacba4dfcb494a6e9
Jonathon Foo <jonfoo108@gmail.com>
2024-04-29 23:27:30 +1000

    Sign-up and Login pages created Need to create function for Database Test the link between pages Redo the FXML as not what we want design wise

--- src/main/java/com/wellnessapp/Main.java
+++ src/main/java/com/wellnessapp/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
     public static final int HEIGHT = 550;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Signup-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
         stage.setTitle(TITLE);
         stage.setScene(scene);
