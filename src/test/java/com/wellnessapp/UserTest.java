783a09beae03d97d2832af6a46be1408250c1f69
Patrick <42367024+Patadam@users.noreply.github.com>
2024-04-30 13:14:37 +1000

    test: move test to tests folder

--- src/test/java/com/wellnessapp/UserTest.java
+++ src/test/java/com/wellnessapp/UserTest.java
@@ -1,18 +1,28 @@
 package com.wellnessapp;
 
 import com.wellnessapp.model.User;
-import org.junit.jupiter.api.*;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class UserTest {
     private User user;
-
     @BeforeEach
     public void setUp() {
-        user = new User("John", "password");
+        user = new User("example@example.com", "account123");
     }
     @Test
-    public void testGetUsername() {
-        String username = user.getUsername();
-        assertEquals("John", username);
+    public void testGetEmail() {
+        assertEquals("example@example.com", user.getEmail());
     }
+    @Test
+    public void testGetPassword() {
+        assertEquals("account123", user.getPassword());
+    }
+//    @Test
+//    public void testSetEmail() {
+//        user.setEmail("jane.smith@example.com");
+//        assertEquals("jane.smith@example.com", contact.getEmail());
+//    }
+
 }
