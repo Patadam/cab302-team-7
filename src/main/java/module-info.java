19224d881228edebfc93077d961927f58f877a2f
Patrick <42367024+Patadam@users.noreply.github.com>
2024-05-14 20:35:30 +1000

    build: update exports and requires

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.wellnessapp {
     requires javafx.fxml;
     requires java.sql;
     requires org.controlsfx.controls;
+    requires java.desktop;
 
     opens com.wellnessapp.controller to javafx.fxml;
     opens com.wellnessapp to javafx.fxml;
@@ -10,4 +11,7 @@ module com.wellnessapp {
     exports com.wellnessapp;
     exports com.wellnessapp.controller;
     exports com.wellnessapp.model;
+    exports com.wellnessapp.model.notice;
+    exports com.wellnessapp.model.hydration;
+    exports com.wellnessapp.model.mood;
 }
