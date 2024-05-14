b2ac80f8b0d9ab7df2eb06587fcc6a17b846a924
Patrick <42367024+Patadam@users.noreply.github.com>
2024-05-14 20:30:30 +1000

    feat: add minimise to tray as BaseController constructor.

--- src/main/java/com/wellnessapp/controller/BaseController.java
+++ src/main/java/com/wellnessapp/controller/BaseController.java
@@ -1,13 +1,29 @@
 package com.wellnessapp.controller;
 
+import com.wellnessapp.services.TrayService;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class BaseController implements IBaseController {
-    @FXML private VBox base;
+    @FXML private Pane base;
     private Stage stage;
 
+    /**
+     * Called on initialisation of any controller inheriting from BaseController.
+     * Forces application to minimise to tray when closed without exiting from system tray.
+     */
+    public BaseController() {
+        onMount(()->{
+            getStage().setOnCloseRequest((event) -> TrayService.handleCloseWithSystemTray(getStage()));
+        });
+    }
+
+    /**
+     * Gets the stage instance by using required "base" fx:id attribute in all FXML.
+     * @return The current stage connected to the controller.
+     */
     @Override
     public Stage getStage() {
         try {
